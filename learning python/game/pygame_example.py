9c1c5e08d0430db29eaf540e4496b2465ea61766
Oliwer Stryjewski <olsskko@gmail.com>
2022-06-20 22:13:02 +0200

    Made simple bullet logic.

--- learning python/game/pygame_example.py
+++ learning python/game/pygame_example.py
@@ -30,6 +30,19 @@ enemyY_change = 64
 def enemy(x, y):
     screen.blit(enemy_img, (x, y))
 
+#Bullet logic
+bullet_img = pygame.image.load("bullet.png")
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 1
+bullet_state = "ready"
+
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bullet_img, (x + 16, y + 10))
+
 # Game loop
 running = True
 while running:
@@ -47,6 +60,8 @@ while running:
                 playerX_change = -0.3
             if event.key == pygame.K_RIGHT:
                 playerX_change = 0.3
+            if event.key == pygame.K_SPACE  :
+                fire_bullet(playerX, bulletY)
             
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -62,7 +77,10 @@ while running:
     if enemyX >= 736:
         enemyX_change = -0.2
         enemyY += enemyY_change
-    
+    #Bullet movement
+    if bullet_state is "fire":
+        fire_bullet(playerX, bulletY)
+        bulletY -= bulletY_change
     playerX += playerX_change
     enemyX += enemyX_change
 
