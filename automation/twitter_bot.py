c8843f64130951a8c844dcbf5d2d8806ab35c7fa
Oliwer Stryjewski <olsskko@gmail.com>
2022-07-08 17:59:35 +0200

    Made a bot which puts username and password into twitter

--- automation/twitter_bot.py
+++ automation/twitter_bot.py
@@ -1,15 +1,31 @@
-from re import S
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import time
-def test_driver_manager_chrome():
-    service = ChromeService(executable_path=ChromeDriverManager().install())
 
-    driver = webdriver.Chrome(service=service)
 
-    time.sleep(3)
-
-    driver.quit()
-
-test_driver_manager_chrome()
+class Twitter_Bot():
+    
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        service = ChromeService(executable_path=ChromeDriverManager().install())
+        self.bot = webdriver.Chrome(service=service)
+    def login(self):
+        bot = self.bot
+        bot.get("https://twitter.com/i/flow/login")
+        time.sleep(5)
+        email = bot.find_element(By.NAME, "text")
+        bot.implicitly_wait(10)
+        email.clear
+        email.send_keys(self.username)
+        email.send_keys(Keys.RETURN)
+        password = bot.find_element(By.NAME, "password")
+        password.clear
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+        time.sleep(10000)
+ed = Twitter_Bot("email","pass")
+ed.login()
