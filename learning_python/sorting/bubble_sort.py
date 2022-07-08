d3ec7f320901020fa7685d25bbe9714d0e191ddb
Oliwer Stryjewski <olsskko@gmail.com>
2022-07-08 09:40:05 +0200

    Made bubble sort

---
+++ learning_python/sorting/bubble_sort.py
@@ -0,0 +1,12 @@
+def bubble_sort(array):
+    n = len(array)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+            print(array)
+
+
+
+
+print(bubble_sort([8, 42, 6,]))
