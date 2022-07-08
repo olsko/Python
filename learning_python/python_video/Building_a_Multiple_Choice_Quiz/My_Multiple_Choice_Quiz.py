73c3072883e6f8aeaf3a7b6d15e395ee61dfa124
Oliwer Stryjewski <olsskko@gmail.com>
2022-07-08 09:17:27 +0200

    Made my_multiple_choice_quiz.py

---
+++ learning_python/python_video/Building_a_Multiple_Choice_Quiz/My_Multiple_Choice_Quiz.py
@@ -0,0 +1,13 @@
+from random import choices
+
+
+questions = ["How many months are there?", ]
+choices = []
+def quiz(questions, choices):
+    for question in len(range(questions)):
+        print_question(questions[question], )
+
+def print_question(q, ch):
+    print(q)
+
+quiz(questions, 1)
