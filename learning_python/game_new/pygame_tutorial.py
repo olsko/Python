a19c0a1f35860ec6a6efe2261adb263d963fc753
Oliwer Stryjewski <olsskko@gmail.com>
2022-10-18 20:22:28 +0200

    Collision detection using mouse position

--- learning_python/game_new/pygame_tutorial.py
+++ learning_python/game_new/pygame_tutorial.py
@@ -17,11 +17,15 @@ snail_rect = snail_surf.get_rect(midbottom = (600,300))
 player_surf = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80,300))
 
+count = 0
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        #if event.type == pygame.MOUSEMOTION:
+        #    if player_rect.collidepoint(event.pos): print("collision")
     
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
@@ -31,8 +35,12 @@ while True:
     screen.blit(snail_surf,snail_rect)
     screen.blit(player_surf,player_rect)
     
-    if player_rect.colliderect(snail_rect):
-        print("colllision")
+    #if player_rect.colliderect(snail_rect):
+    #    print("colllision")
+    
+    #mouse_pos = pygame.mouse.get_pos()
+    #if player_rect.collidepoint((mouse_pos)):
+    #    print( "collision")
 
     pygame.display.update()
     clock.tick(60)
